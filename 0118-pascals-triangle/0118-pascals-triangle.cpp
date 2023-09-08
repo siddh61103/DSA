25c264af6d9d975d46b25011cbc2c14a2ba3129d
Siddharth <96472929+siddh61103@users.noreply.github.com>
2023-09-08 15:02:25 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (6.18%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
+    void pascal(vector<vector<int>> &ans,int n,int a){
+        int k = ans.size()-1;
+        vector<int> temp;
+        if(a==n){
+            return ;
+        }
+        for(int j=0;j<a+1;j++){
+            if(j==0 || j==a){
+                temp.push_back(1);
+            }
+            else{
+                temp.push_back(ans[k][j-1]+ans[k][j]);
+            }
+        }
+        ans.push_back(temp);
+        pascal(ans,n,a+1);
+        return;
+    }
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> ans;
-        for(int i=0;i<numRows;i++){
-            vector<int> random;
-            for(int j=0;j<i+1;j++){
-                if(j==0 || j==i){
-                    random.push_back(1);
-                }
-                else{
-                    random.push_back(ans[i-1][j-1]+ans[i-1][j]);
-                }
-            }
-            ans.push_back(random);
-        }
+        pascal(ans,numRows,0);
         return ans;
-    } 
+    }
 };
