cb6e13c0063f7555138d8eadbb705a29b66cb49a
Siddharth <96472929+siddh61103@users.noreply.github.com>
2023-09-10 21:38:53 +0530

    Time: 8 ms (11.02%), Space: 8.4 MB (96.27%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -2,14 +2,8 @@ class Solution {
 public:
     int hIndex(vector<int>& citations) {
         int n = citations.size();
-        // if(n==1){
-        //     return citations[0];
-        // }
-        sort(citations.begin(),citations.end());
         
-        for(int i = 0; i >= 0;i--){
-            cout<<citations[i]<<" ";
-        }
+        sort(citations.begin(),citations.end());
         
         for(int i = citations[n-1]; i >= 0;i--){
             int count = 0;
