209f4abec23e388c8571f0e33a3563ccf391a544
Siddharth <96472929+siddh61103@users.noreply.github.com>
2023-12-05 21:44:01 +0530

    Time: 2 ms (44.05%), Space: 6.2 MB (73.09%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -4,14 +4,8 @@ public:
         int count = n;
         int matches = 0;
         while(count!=1){
-            if(count % 2 == 0 ){
-                matches = matches + count/2;
-                count = count/2;
-            }
-            else{
-                matches = matches + count/2;
-                count = count/2 +1;
-            }
+            matches = matches + count/2;
+            count = count/2 + count%2;
         }
         return matches;
     }
