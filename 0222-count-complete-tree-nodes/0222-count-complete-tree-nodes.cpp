bb99ff4fda1183c58519d78ac3cdc99eaa62efa1
Siddharth <96472929+siddh61103@users.noreply.github.com>
2023-12-17 15:38:03 +0530

    Time: 33 ms (8.45%), Space: 31.1 MB (50.80%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
-    void inorder(TreeNode* root,int &ans){
-        if(root==NULL) return;
-        inorder(root->left,ans);
-        ans++;
-        inorder(root->right,ans);
+    
+    int left_height(TreeNode* root){
+        if(root==NULL) return 0 ;
+        return 1+left_height(root->left);
+    }
+    // Different method in right and left function both are right (recusrion or while loop)
+    int right_height(TreeNode* root){
+        int h =0;
+        while(root){
+            root = root->right;
+            h++;
+        }
+        return h;
     }
     int countNodes(TreeNode* root) {
-        int ans = 0;
-        inorder(root,ans);
-        return ans;
+        if(root==NULL) return 0 ;
         
+        int lh = left_height(root);
+        
+        int rh = right_height(root);
+        
+        if(lh==rh) return (1<<lh)-1;
+        return 1 + countNodes(root->left) + countNodes(root->right); 
     }
 };
