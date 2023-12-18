1b53a9db9666ac51ffa75bbf267f0fdf2461f969
Siddharth <96472929+siddh61103@users.noreply.github.com>
2023-12-18 11:24:59 +0530

    Time: 0 ms (100.00%), Space: 8.9 MB (19.66%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -1,36 +1,30 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+//////////////////Morris Traversal/////////////////////
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        if(root==NULL){
-            return ans;
-        }
-        stack<TreeNode*>s;
-        TreeNode* node = root;
-        while(true){
-            if(node!=NULL){
-                s.push(node);
-                node = node->left;
+        vector<int> inorder;
+        TreeNode* curr = root;
+        while(curr!=NULL){
+            if(curr->left==NULL){
+                inorder.push_back(curr->val);
+                curr = curr->right;
             }
             else{
-                if(s.empty()) break;
-                node = s.top();
-                s.pop();
-                ans.push_back(node->val);
-                node = node->right;
+                TreeNode* prev = curr->left;
+                while(prev->right && prev->right!=curr){
+                    prev = prev->right;
+                }
+                if(prev->right==NULL){
+                    prev->right = curr;
+                    curr = curr->left;
+                }
+                else{
+                    prev->right = NULL;
+                    inorder.push_back(curr->val);
+                    curr= curr->right;
+                }
             }
         }
-        return ans;
+        return inorder;
     }
 };
