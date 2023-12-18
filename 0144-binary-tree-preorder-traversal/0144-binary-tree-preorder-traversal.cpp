ebed4513c89c6a8158f5a44aa2a09999f2696673
Siddharth <96472929+siddh61103@users.noreply.github.com>
2023-12-18 11:42:11 +0530

    Time: 4 ms (33.90%), Space: 8.8 MB (54.10%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -1,31 +1,30 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+//////////////////Morris Traversal/////////////////////
 class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        if(root==NULL){
-            return ans;
+        vector<int> preorder;
+        TreeNode* curr = root;
+        while(curr!=NULL){
+            if(curr->left==NULL){
+                preorder.push_back(curr->val);
+                curr = curr->right;
+            }
+            else{
+                TreeNode* prev = curr->left;
+                while(prev->right && prev->right!=curr){
+                    prev = prev->right;
+                }
+                if(prev->right==NULL){
+                    prev->right = curr;
+                    preorder.push_back(curr->val);
+                    curr = curr->left;
+                }
+                else{
+                    prev->right = NULL;
+                    curr= curr->right;
+                }
+            }
         }
-        stack<TreeNode*> s;
-        s.push(root);
-        while(!s.empty()){
-            root = s.top();
-            s.pop();
-            ans.push_back(root->val);
-            if(root->right!=NULL) s.push(root->right);
-            if(root->left!=NULL) s.push(root->left);
-        }
-        return ans;
-        
+        return preorder;
     }
 };
