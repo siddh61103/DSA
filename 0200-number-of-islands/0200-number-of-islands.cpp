4059977e7c3daac1202227dce8da5e7c78307704
Siddharth <96472929+siddh61103@users.noreply.github.com>
2024-01-07 22:37:26 +0530

    Time: 30 ms (61.17%), Space: 19.8 MB (13.09%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
-    void dfs(int row, int col,vector<vector<int>> &vis,vector<vector<char>>& grid){
+    void bfs(int row, int col,vector<vector<int>> &vis,vector<vector<char>>& grid){
+        queue<pair<int,int>> q;
+        q.push({row,col});
+        // if it can move in 8 directions
+        // int drow[] = {-1,-1,0,1,1,1,0,-1};
+        // int dcol[] = {0, 1, 1,1,0,-1,-1,-1};
         
-        vis[row][col]++;
+        // if it can move in 4 directions
         int drow[] = {-1,0,1,0};
         int dcol[] = {0,1,0,-1};
-        
-        for(int i=0;i<4;i++){
-            int nrow = row + drow[i];
-            int ncol = col + dcol[i];
-            if(nrow>=0 && nrow<grid.size() && ncol>=0 && ncol<grid[0].size() 
-               && vis[nrow][ncol]==0 && grid[nrow][ncol]=='1'){
-                dfs(nrow,ncol,vis,grid);
+        while(!q.empty()){ 
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+            vis[row][col] = 1;
+            for(int i=0;i<4;i++){
+                int nrow = row + drow[i];
+                int ncol = col + dcol[i];
+                if(nrow>=0 && nrow<grid.size() && ncol>=0 && ncol<grid[0].size() 
+                   && vis[nrow][ncol]==0 && grid[nrow][ncol]=='1'){
+                    vis[nrow][ncol]=1;
+                    q.push({nrow,ncol});
+                }
             }
         }
         return ;
@@ -25,7 +36,7 @@ public:
             for(int j = 0;j<m;j++){
                 if(vis[i][j]==0 && grid[i][j]=='1'){
                     cnt++;
-                    dfs(i, j, vis, grid);
+                    bfs(i, j, vis, grid);
                 }
             }
         }
