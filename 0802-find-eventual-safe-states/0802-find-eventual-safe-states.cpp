a811eecc2d257ec84b1086b73072adc3de3b1b3f
Siddharth <96472929+siddh61103@users.noreply.github.com>
2024-01-11 20:34:06 +0530

    Time: 165 ms (43.06%), Space: 64.1 MB (10.62%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,35 +1,41 @@
 class Solution {
-    bool dfs(int node,vector<int>&vis,vector<vector<int>>& graph){
-        vis[node] = 2;
-        for(auto it : graph[node]){
-            if(vis[it]==0){
-                if( dfs(it,vis,graph) ){
-                    vis[node] = 3;
-                    return true;
-                };
-            }
-            else if(vis[it]==2 ||vis[it]==3){
-                vis[node] = 3;
-                return true;
-            }
-        }
-        vis[node] = 1;
-        return false;
-    }
     
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<int> ans;
-        vector<int> vis(n,0);
-        for(int i = 0 ;i<n;i++){
-            if(vis[i]==0){
-                dfs(i,vis,graph);
+        vector<vector<int>> adj(n);
+        for(int i =0;i<graph.size();i++){
+            for(auto it : graph[i]){
+                adj[it].push_back(i);
             }
         }
-        for(int i = 0 ;i<n;i++){
-            if(vis[i]!=3) ans.push_back(i);
+        graph = adj;
+        vector<int> indegree(n,0);
+        
+        for(int i = 0;i<n;i++){
+            for(auto j : graph[i]){
+                indegree[j]++;
+            }
         }
+        
+        queue<int> q;
+        for(int i =0;i<n;i++){
+            if(indegree[i]==0) q.push(i);
+        }
+        
+        vector<int>ans;
+        
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto i : graph[node]){
+                indegree[i]--;
+                if(indegree[i]==0) q.push(i);
+            }
+        }
+        sort(ans.begin(),ans.end());
         return ans;
+        
     }
 };
