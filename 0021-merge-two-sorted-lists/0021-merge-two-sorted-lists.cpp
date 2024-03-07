0b6d80d0ac71f7c39f3b99d6c921a330cd3060b4
Siddharth <96472929+siddh61103@users.noreply.github.com>
2024-03-07 11:52:50 +0530

    Time: 5 ms (49.87%), Space: 18.4 MB (11.79%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -8,51 +8,35 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-ListNode* merge(ListNode* &first, ListNode* &second) {
-    if(first->next == NULL){
-        first->next = second;
-        return first;
-    }
-    ListNode* curr1 = first;
-    ListNode* next1 = first->next;
-    ListNode* curr2 = second;
-    ListNode* next2 = second->next;
-    while(next1!=NULL && curr2!=NULL){
-        if((curr1->val)<=(curr2->val) && (curr2->val)<=(curr1->next->val)){
-            //adding node
-            next2 = curr2->next;
-            curr2->next = curr1->next;
-            curr1->next = curr2;
-            //updating pointers
-            curr1 = curr2;
-            curr2 = next2;
-        }
-        else{
-            curr1 = curr1->next;
-            next1 = curr1->next;
-            if(curr1->next==NULL){
-                curr1->next = curr2;
-                return first;
-            }
-        }
-    }
-    return first;
-}
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(list1==NULL){
-            return list2;
+        ListNode* i = list1;
+        ListNode* j = list2;
+        ListNode* ans = new ListNode(0);
+        ListNode* temp = ans;
+        while(i && j){
+            if(i->val <= j->val){
+                temp->next = i;
+                temp = temp->next;
+                i = i->next;
+            }
+            else{
+                temp->next = j;
+                temp = temp->next;
+                j = j->next;
+            }
         }
-        if(list2==NULL){
-            return list1;
+        while(i){
+                temp->next = i;
+                temp = temp->next;
+                i = i->next;
         }
-        if(list1->val<=list2->val){
-            return merge(list1,list2);
+        while(j){
+                temp->next = j;
+                temp = temp->next;
+                j = j->next;
         }
-        else{
-            return merge(list2,list1);
-        }
-        
+        return ans->next;
     }
 };
