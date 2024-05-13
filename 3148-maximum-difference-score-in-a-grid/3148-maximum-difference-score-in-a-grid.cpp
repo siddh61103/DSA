fc29876192612e23f26d2fdd7efb189c26a6759a
Siddharth <96472929+siddh61103@users.noreply.github.com>
2024-05-13 18:30:57 +0530

    Time: 149 ms (14.29%), Space: 62.4 MB (71.43%) - LeetHub

--- 3148-maximum-difference-score-in-a-grid/3148-maximum-difference-score-in-a-grid.cpp
+++ 3148-maximum-difference-score-in-a-grid/3148-maximum-difference-score-in-a-grid.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
-    int solve(vector<vector<int>>& grid, int r, int c, vector<vector<int>>& dp){
-        if(dp[r][c]!=-1e9) return max(0,dp[r][c]);
-        int a = 0;
-        int i = -1e9;
-        int j = -1e9;
-        int node = grid[r][c];
-        if(r+1 < grid.size()){
-            a = grid[r+1][c] - grid[r][c];
-            i = a + solve(grid, r+1, c, dp);
-        }
-        if(c+1 < grid[0].size()){
-            a = grid[r][c+1] - grid[r][c];
-            j = a + solve(grid, r, c+1, dp);
-        }
-        dp[r][c] = max(i,j);
-        return max(0,dp[r][c]);
-    }
     int maxScore(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
         vector<vector<int>>dp(n,vector<int>(m,-1e9));
+        dp[n-1][m-1]=0;
         int ans = -1e9;
-        solve(grid,0,0,dp);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                ans = max(ans,dp[i][j]);
+        for(int r=n-1; r>=0; r--){
+            for(int c=m-1; c>=0; c--){
+                int a = 0;
+                int i = -1e9;
+                int j = -1e9;
+                int node = grid[r][c];
+                if(r+1 < n){
+                    a = grid[r+1][c] - grid[r][c];
+                    i = a + dp[r+1][c];
+                }
+                if(c+1 < m){
+                    a = grid[r][c+1] - grid[r][c];
+                    j = a + dp[r][c+1];
+                }
+                ans = max(ans,max(i,j));
+                dp[r][c] = max(0,max(i,j));
             }
         }
         return ans;
