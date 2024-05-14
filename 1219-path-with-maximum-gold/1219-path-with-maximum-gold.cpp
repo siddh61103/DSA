002d216676372cc9812195dadb39f662b615d53a
Siddharth <96472929+siddh61103@users.noreply.github.com>
2024-05-14 15:46:17 +0530

    Time: 132 ms (60.79%), Space: 9.2 MB (63.16%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    int solve(int row, int col, vector<vector<int>>& grid, vector<vector<int>> &dp){
-        int n = grid.size();
-        int m = grid[0].size();
-        int drow[] = {1,0,-1,0};
-        int dcol[] = {0,1,0,-1};
+    vector<int> drow = {1,0,-1,0};
+    vector<int> dcol = {0,1,0,-1};
+    
+    int solve(int row, int col, vector<vector<int>>& grid, int n, int m){
         int a = 0;
+        int curr = grid[row][col];
+        grid[row][col] = 0;
         for(int i=0; i<4; i++){
             int r = row + drow[i];
             int c = col + dcol[i];
-            if(r>=0 && c>=0 && r<n && c<m && dp[r][c]==-1 && grid[r][c]!=0){
-                dp[r][c] = 0;
-                int m  = solve(r, c, grid, dp);
-                dp[r][c] = -1;
-                a = max(a,m);
+            if(r>=0 && c>=0 && r<n && c<m && grid[r][c]!=0){
+                a = max(a, solve(r, c, grid, n, m));
             }
         }
-        return dp[row][col] = a + grid[row][col];
+        grid[row][col] = curr;
+        return a + curr;
     }
     int getMaximumGold(vector<vector<int>>& grid) {
         int n = grid.size();
@@ -25,10 +24,7 @@ public:
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==0) continue;
-                vector<vector<int>> dp(n,vector<int>(m,-1));
-                dp[i][j] = 0;
-                int k = solve(i, j, grid, dp);
-                ans = max(ans,k);
+                ans = max(ans, solve(i, j, grid, n, m));
             }
         }
         return ans;
