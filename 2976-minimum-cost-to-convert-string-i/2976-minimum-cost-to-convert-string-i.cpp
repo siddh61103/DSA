9589551b03ebc042e46ea73b65e0ae9c2a72bb68
Siddharth <96472929+siddh61103@users.noreply.github.com>
2024-07-27 22:38:30 +0530

    Time: 179 ms (58.05%), Space: 99.1 MB (47.68%) - LeetHub

--- 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.cpp
+++ 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.cpp
@@ -1,41 +1,27 @@
 class Solution {
 public:
-    void solve(int n,vector<int> &vis, vector<vector<long long>> &vec,long long sum,vector<long long> & dis) {
-        for(int i=0;i<26;i++){
-            if(i==n || vis[i]==1) continue;
-            if(vec[n][i]!=-1){
-                if(sum+vec[n][i]<dis[i]){
-                    vis[i]=1;
-                    dis[i] = sum+vec[n][i];
-                    solve(i,vis,vec,dis[i],dis);
-                    vis[i]=0;
-                }
-            }
-        }
-        return;
-    }
     long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
         vector<vector<long long>> vec(26,vector<long long>(26,1e18));
-        vector<vector<long long>> adj;
         for(int i=0;i<original.size();i++){
             int a = original[i]-'a';
             int b = changed[i]-'a';
-            vec[a][b] = min(vec[a][b], (long long)cost[i]);
+            vec[a][b] = min(vec[a][b],(long long) cost[i]);
         }
+        for(int i=0;i<26;i++) vec[i][i]=0;
         for(int i=0;i<26;i++){
-            vector<int> vis(26,0);
-            vis[i]=1;
-            vector<long long> dis(26,1e18);
-            solve(i,vis,vec,0,dis);
-            adj.push_back(dis);
+            for(int j=0;j<26;j++){
+                for(int k=0;k<26;k++){
+                    vec[j][k] = min(vec[j][k],vec[j][i]+vec[i][k]);
+                }
+            }
         }
         long long ans = 0;
         for(int i=0;i<source.size();i++){
             int a = source[i]-'a';
             int b = target[i]-'a';
             if(a==b) continue;
-            if(adj[a][b]>=1e18) return -1;
-            ans +=adj[a][b];
+            if(vec[a][b]>=1e18) return -1;
+            ans +=vec[a][b];
         }
         return ans;
     }
